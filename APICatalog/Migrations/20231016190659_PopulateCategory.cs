63dd12c805bc60d86fa3a9942270b019350c1d6f
Caio Molinaro <cmolinaro19@gmail.com>
2023-10-16 16:17:31 -0300

    initial commit

---
+++ APICatalog/Migrations/20231016190659_PopulateCategory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas','bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Lanches','lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas','sobremesas.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
