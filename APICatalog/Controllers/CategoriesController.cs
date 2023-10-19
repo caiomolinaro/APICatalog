565c32e3a8a650d0ac05d5a68961e4f28be6a7cf
Caio Molinaro <cmolinaro19@gmail.com>
2023-10-19 16:51:26 -0300

    error handling

--- APICatalog/Controllers/CategoriesController.cs
+++ APICatalog/Controllers/CategoriesController.cs
@@ -21,7 +21,14 @@ namespace APICatalog.Controllers
         [HttpGet("products")] //define uma outra rota, tem que informar o nome do controlador/products para acessar
         public ActionResult<IEnumerable<CategoryModel>> GetCategoriesProducts()
         {
-            return _dbContext.Categories.AsNoTracking().Include(p => p.Products).ToList(); //o include inclui os produtos
+            try
+            {
+                return _dbContext.Categories.AsNoTracking().Include(p => p.Products).ToList(); //o include inclui os produtos
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
 
         }
 
@@ -29,67 +36,101 @@ namespace APICatalog.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<CategoryModel>> Get()
         {
-            return _dbContext.Categories.AsNoTracking().ToList(); //so retorna as categorias, o as no tracking retorna uma nova consulta e não é
-                                                                  //amarzenada no cache pelo context, otimiza a consulta e
-                                                                  //só deve ser usado em consultas somente leitura (get)
+            try
+            { 
+                return _dbContext.Categories.AsNoTracking().ToList(); //so retorna as categorias, o as no tracking retorna uma nova consulta e não é
+                                                                      //amarzenada no cache pelo context, otimiza a consulta e
+            }                                                         //só deve ser usado em consultas somente leitura (get)
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //get que retorna a categoria pelo id
         [HttpGet("{id:int}", Name = "ObtainCategory")]
         public ActionResult<CategoryModel> Get(int id)
         {
-            var category = _dbContext.Categories.AsNoTracking().FirstOrDefault(p => p.CategoryId == id);
-
-            if (category == null)
+            try
             {
-                return NotFound();
+                var category = _dbContext.Categories.AsNoTracking().FirstOrDefault(p => p.CategoryId == id);
+
+                if (category == null)
+                {
+                    return NotFound($"A categoria com o id: {id} não foi encontrada");
+                }
+                return Ok(category);
             }
-            return Ok(category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //post que cria uma categoria
         [HttpPost]
         public ActionResult Post(CategoryModel category)
         {
-            if (category is null)
+            try
             {
-                return BadRequest();
+                if (category is null)
+                {
+                    return BadRequest();
+                }
+
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+
+                return new CreatedAtRouteResult("ObtainCategory", new { id = category.CategoryId }, category);
             }
-
-            _dbContext.Categories.Add(category);
-            _dbContext.SaveChanges();
-
-            return new CreatedAtRouteResult("ObtainCategory", new { id = category.CategoryId }, category);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //put que atualiza uma categoria
         [HttpPut("{id:int}")]
         public ActionResult put(int id, CategoryModel category)
         {
-            if (id != category.CategoryId)
+            try
             {
-                return BadRequest();
-            }
+                if (id != category.CategoryId)
+                {
+                    return BadRequest("Os dados informados, são inválidos");
+                }
 
-            _dbContext.Entry(category).State = EntityState.Modified; //altera o estado pra modified/modificado
-            _dbContext.SaveChanges();
-            return Ok(category);
+                _dbContext.Entry(category).State = EntityState.Modified; //altera o estado pra modified/modificado
+                _dbContext.SaveChanges();
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //metodo delete que deleta uma categoria
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            var category = _dbContext.Categories.FirstOrDefault(p => p.CategoryId == id);
-            
-            if (category == null)
+            try
             {
-                return NotFound();
-            }
+                var category = _dbContext.Categories.FirstOrDefault(p => p.CategoryId == id);
 
-            _dbContext.Categories.Remove(category);
-            _dbContext.SaveChanges();
-            return Ok();
+                if (category == null)
+                {
+                    return NotFound($"A categoria com o id: {id} não foi encontrada");
+                }
+
+                _dbContext.Categories.Remove(category);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
     }
 }
--- APICatalog/Controllers/ProductsController.cs
+++ APICatalog/Controllers/ProductsController.cs
@@ -27,12 +27,19 @@ namespace APICatalog.Controllers
             var products = _dbContext.Products.AsNoTracking().ToList();
 
             //verifica se os produtos não são nulls
-            if(products is null)
+            try
             {
-                return NotFound("Produtos não encontrados"); //equivalente ao codigo http 404 - não encontrado
-            }
+                if (products is null)
+                {
+                    return NotFound("Produto não encontrado"); //equivalente ao codigo http 404 - não encontrado
+                }
 
-            return products;
+                return products;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //metodo get que retorna um produto pelo id
@@ -40,67 +47,94 @@ namespace APICatalog.Controllers
         public ActionResult<ProductModel> Get(int id)
         {
             //armazena o produto por id na var product
-            var product = _dbContext.Products.AsNoTracking().FirstOrDefault(p => p.ProductId == id);
-
-            if (product is null)
+            try
             {
-                return NotFound("Produto não encontrado");
+                var product = _dbContext.Products.AsNoTracking().FirstOrDefault(p => p.ProductId == id);
+
+                if (product is null)
+                {
+                    return NotFound("Produto não encontrado");
+                }
+                return product;
             }
-            return product;
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //metodo post que cria um novo produto
         [HttpPost]
         public ActionResult Post(ProductModel product) 
         {
-            if(product is null)
+            try
             {
-                return BadRequest();
+                if (product is null)
+                {
+                    return BadRequest();
+                }
+
+                //cria e persiste o novo produto na tabela
+                _dbContext.Products.Add(product);
+                _dbContext.SaveChanges();
+
+                return new CreatedAtRouteResult("ObtainProduct", new { id = product.ProductId }, product); // retorna o http 202 created
             }
-
-            //cria e persiste o novo produto na tabela
-            _dbContext.Products.Add(product);
-            _dbContext.SaveChanges();
-
-            return new CreatedAtRouteResult("ObtainProduct", new { id = product.ProductId }, product); // retorna o http 202 created
-
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //metodo put que atualiza um produto existente
         [HttpPut("{id:int}")] //valor mapeado para o parametro do metodo id int
         public ActionResult Put(int id, ProductModel product)
         {
-            if(id != product.ProductId)
+            try
             {
-                return BadRequest(); //se for diferente o id retorna o badrequest http 400
+                if (id != product.ProductId)
+                {
+                    return BadRequest(); //se for diferente o id retorna o badrequest http 400
+                }
+
+                //identifica que vai ser modificado e salva
+                _dbContext.Entry(product).State = EntityState.Modified; //identifica que vai ser modificado e salva
+                _dbContext.SaveChanges();
+
+                return Ok(product);
             }
-
-            //identifica que vai ser modificado e salva
-            _dbContext.Entry(product).State = EntityState.Modified; //identifica que vai ser modificado e salva
-            _dbContext.SaveChanges();
-
-            return Ok(product);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
 
         //metodo delete que deleta um produto existente
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            //localiza o produto pelo id
-            var product = _dbContext.Products.FirstOrDefault(p => p.ProductId == id);   
-
-            //verifica se o produto é null
-            if(product is null)
+            try
             {
-                return NotFound("Produto não foi localizado"); //http 404
+                //localiza o produto pelo id
+                var product = _dbContext.Products.FirstOrDefault(p => p.ProductId == id);
+
+                //verifica se o produto é null
+                if (product is null)
+                {
+                    return NotFound("Produto não foi localizado"); //http 404
+                }
+
+                //se o produto for localizado então é removido
+
+                _dbContext.Products.Remove(product);
+                _dbContext.SaveChanges();
+
+                return Ok(); //retorna o http 200
             }
-
-            //se o produto for localizado então é removido
-
-            _dbContext.Products.Remove(product);
-            _dbContext.SaveChanges();
-
-            return Ok(); //retorna o http 200
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema");
+            }
         }
     }
 }
